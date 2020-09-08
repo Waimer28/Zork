f122fff56040a1c7ffdc8cf885e9b85af04d89f7
Waimer28 <55116913+Waimer28@users.noreply.github.com>
2020-09-08 19:19:37 -0400

    1.1

---
+++ Zork/Zork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
+        }
+    }
+}
