7b5eb35cca07eab92bc8394ddf150af9e0fa019a
Waimer28 <55116913+Waimer28@users.noreply.github.com>
2020-09-09 02:36:29 -0400

    1.3

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -7,23 +7,42 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write(">");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.WEST:
+                    case Commands.EAST:
+                        outputString = $"You moved {command}";
+                        break;
+
+                    default:
+                        outputString = "Unknown command";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            } 
 
         }
 
-        private static Commands ToCommand(string commandString)
+        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        {
-            if (Enum.TryParse<Commands>(commandString, true, out Commands result))
-            {
-                return result;
-            }
-            else
-            {
-                return Commands.UNKNOWN;
-            }
-        }
     }
 }
