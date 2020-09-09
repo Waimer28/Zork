6b43d70433b56a811ffdea7413abdd901c4412fd
Waimer28 <55116913+Waimer28@users.noreply.github.com>
2020-09-09 02:04:09 -0400

    1.2

---
+++ Zork/Zork/Commands.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Zork
+{
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+}
--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -8,18 +8,21 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            if (inputString == "QUIT")
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+
+        }
+
+        private static Commands ToCommand(string commandString)
+
+        {
+            if (Enum.TryParse<Commands>(commandString, true, out Commands result))
             {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door");
+                return result;
             }
             else
             {
-                Console.WriteLine("Unrecognized command.");
+                return Commands.UNKNOWN;
             }
         }
     }
