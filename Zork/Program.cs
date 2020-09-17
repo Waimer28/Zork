40add471de742abd8a6c11f4dae044af9546636e
Waimer28 <55116913+Waimer28@users.noreply.github.com>
2020-09-16 20:02:26 -0400

    Zork 2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,85 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-
-           
-           
-
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.WriteLine(Rooms[CurrentRoomIndex]);                
+                Console.WriteLine(Rooms[CurrentRoomIndex]);
                 Console.Write(">");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door";
+                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door");
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = Move(command) ? $"You moved {command}": "The way is shut!";
+                        if (Move(command) == false)
+                        {
+                            Console.WriteLine("The way is shut!");
+                        }
                         break;
 
                     default:
-                        outputString = "Unknown command";
+                        Console.WriteLine("Unknown command.");
                         break;
                 }
-
-
-                Console.WriteLine(outputString);
-            } 
-
+            }
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
         private static bool Move(Commands command)
-        {
-            bool didMove = false;
 
+        {
+            bool isValidMove = true;
             switch (command)
             {
                 case Commands.EAST when CurrentRoomIndex < Rooms.Length - 1:
-                    CurrentRoomIndex++;                   
-                    didMove = true;
+                    CurrentRoomIndex++;
                     break;
 
 
                 case Commands.WEST when CurrentRoomIndex > 0:
                     CurrentRoomIndex--;
-                    didMove = true;
+                    break;
+
+                default:
+                    isValidMove = false;
                     break;
 
             }
-                    return didMove;
+            return isValidMove;
         }
-
         private static string[] Rooms =
         {
-             "Forest",
-             "West of House",
-             "Behind House",
-             "Canyon View",
-             "Clearing"
+            "Forest",
+            "West of House",
+            "Behind House",
+            "Clearing",
+            "Canyon"
         };
-
+        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         private static int CurrentRoomIndex = 1;
     }
 }
