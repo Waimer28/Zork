2dd799f302a7a6008cb2f081723a9674b679c975
Waimer28 <55116913+Waimer28@users.noreply.github.com>
2020-10-07 12:05:13 -0400

    Zork 4.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 
 namespace Zork
@@ -13,9 +14,20 @@ namespace Zork
                 return Rooms [Location.Row, Location.Column];
             }
         }
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                RoomMap[room.Name] = room;
+            }
+        }
         static void Main(string[] args)
         {
-            InitializeRoomDescriptions();
+            const string defaultRoomsfilename = "Rooms.txt";
+            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsfilename);
+            InitializeRoomDescriptions(roomsFilename);
+
             Console.WriteLine("Welcome to Zork!");
 
             Room previousRoom =null;
@@ -30,7 +42,6 @@ namespace Zork
                 }
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
-
 
                 string outputString;
                 switch (command)
@@ -57,7 +68,6 @@ namespace Zork
                 Console.WriteLine(outputString);
             }
         }
-
         private static bool Move(Commands command)
         {
             bool isValidMove = true;
@@ -75,7 +85,6 @@ namespace Zork
                     Location.Column++;
                     break;
 
-
                 case Commands.WEST when Location.Column > 0:
                     Location.Column--;
                     break;
@@ -83,36 +92,44 @@ namespace Zork
                 default:
                     isValidMove = false;
                     break;
-
             }
             return isValidMove;
         }
         private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-        private static void InitializeRoomDescriptions()
+        private static readonly Dictionary<string, Room> RoomMap;
+        private enum Fields
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
-            {
-                roomMap[room.Name] = room;
-            }
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with a large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+            Name = 0,
+            Description
         }
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
+        private static void InitializeRoomDescriptions(string roomsFilename)
+        {
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
 
-        private static readonly Room[,] Rooms =
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(fieldDelimiter);
+                if (fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid Record.");
+                }
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
+                RoomMap[name].Description = description;
+            }
+        }                   
+        private static Room[,] Rooms =
         {
              { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
              { new Room("Forest"), new Room("West of House"), new Room ("Behind House") },
              { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
-
         private static readonly List<Commands> Directions = new List<Commands>
         {
             Commands.NORTH,
@@ -120,7 +137,6 @@ namespace Zork
             Commands.EAST,
             Commands.WEST
         };
-
         private static (int Row, int Column) Location = (1,1);
     }
 }
